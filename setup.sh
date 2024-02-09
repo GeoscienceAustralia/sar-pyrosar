f6cdcecc5634066f9aaa26788c92346649a316e8
EC2 Default User <ec2-user@ip-10-0-7-195.ap-southeast-2.compute.internal>
2024-02-09 05:19:28 +0000

    logic to stop credentials

--- setup.sh
+++ setup.sh
@@ -1,5 +1,6 @@
+# build the container
+docker build . -t pyrosar --ulimit nofile=122880:122880
+
 # ignore changes to credentials
 git update-index --assume-unchanged credentials/*
-
-# build the container
-docker build . -t pyrosar
+git update-index --assume-unchanged credentials/.netrc
