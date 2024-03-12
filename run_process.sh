e531dd9ee781fed7eda15b3154d16eef0aec757b
abradley60 <alexbradley60@gmail.com>
2024-03-12 23:50:17 +0000

    Adding useful comments

--- run_process.sh
+++ run_process.sh
@@ -1,1 +1,4 @@
+# assumes all folders referenced in the config are in /data
+# mounts local aws files in the container and uses them for upload
+# changes to this may be required with data stored in different folders
 docker run -v /data:/data -v ${PWD}:/app/ -v $HOME/.aws:/root/.aws -it pyrosar
